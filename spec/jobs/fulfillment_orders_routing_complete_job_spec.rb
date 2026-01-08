e249ade260f81e09258b880cbdd50ca3c056a9b0
Till Taeubrich <Till.taeubrich@gmail.com>
2026-01-08 15:00:55 -0300

    Add test for FulfillmentOrdersRoutingCompleteJob to raise error when shop not found

--- spec/jobs/fulfillment_orders_routing_complete_job_spec.rb
+++ spec/jobs/fulfillment_orders_routing_complete_job_spec.rb
@@ -1,5 +1,11 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 RSpec.describe FulfillmentOrdersRoutingCompleteJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "raises when shop not found" do
+    expect {
+      described_class.perform_now(shop_domain: "unknown.myshopify.com", webhook: {})
+    }.to raise_error(ActiveRecord::RecordNotFound)
+  end
 end
